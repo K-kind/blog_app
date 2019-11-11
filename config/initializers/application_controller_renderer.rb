0772cdd4aafa08dbedf9b4f263a633e40279de4d
K.K <gokennsirou@yahoo.co.jp>
2019-11-11 13:09:38 +0900

    Finish user edit, update, index, and destroy actions

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '19044462e61ab5430be2ef79778f3faa189869f4f107e8cdbc3ae216cdc162e1f3837db6c3e73cb8f44017361452950ce80ed09afca9382e5b78b3cb392983e1'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_135353) do
+ActiveRecord::Schema.define(version: 2019_11_11_033425) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_11_09_135353) do
     t.string "password_digest"
     t.string "profile_image_id"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
