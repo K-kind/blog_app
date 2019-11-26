6bc122efb0060daa1a75a8661c55118854595711
K.K <gokennsirou@yahoo.co.jp>
2019-11-26 19:20:50 +0900

    Enable users to delete themselves

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :following, :followers]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :index]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :admin_user, only: [:index]
 
   def index
     @users = User.where(activated: true).paginate(page: params[:page])
@@ -58,9 +58,14 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy
+    if @user.admin?
+      flash[:danger] = "管理ユーザーは削除することができません"
+      redirect_to root_url
+      return
+    end
+    @user.destroy
     flash[:success] = "User deleted"
-    redirect_to users_url
+    redirect_to root_url
   end
 
   private
@@ -71,7 +76,9 @@ class UsersController < ApplicationController
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      unless current_user?(@user) || current_user.admin? 
+        redirect_to(root_url)
+      end
     end
 
     def admin_user
